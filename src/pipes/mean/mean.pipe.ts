b1eac1557d9e76ffd9263fd6b6461d7fba57f2b8
Heath Lester <h.allen.lester@gmail.com>
2024-03-30 16:47:01 -0400

    handle nan in mean pipe

--- src/pipes/mean/mean.pipe.ts
+++ src/pipes/mean/mean.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MeanPipe implements PipeTransform {
 	transform(number: number | undefined, places: number = 1): string {
-		if (number === undefined) {
+		if (number === undefined || isNaN(number)) {
 			return '0.0';
 		}
 		return number.toFixed(places).toString();
